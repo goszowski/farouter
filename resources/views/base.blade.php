2ba3ae81bf7348bf4300fd46c6d920207b945c5a
Goszowski <goszowski@gmail.com>
2020-12-11 12:36:11 +0200

    added package nprogress

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -8,18 +8,30 @@
     <link rel="preconnect" href="https://fonts.googleapis.com" crossorigin="">
 
     <link rel="stylesheet" href="{{ asset('vendor/farouter/tabler/css/tabler.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/farouter/nprogress/nprogress.css') }}">
 </head>
 <body class="antialiased">
 
     @yield('app')
 
     <script src="{{ asset('vendor/farouter/tabler/js/vendors/jquery-3.2.1.min.js') }}"></script>
+    <script src="{{ asset('vendor/farouter/nprogress/nprogress.js') }}"></script>
     <script src="{{ asset('vendor/farouter/jquery.pjax.min.js') }}"></script>
 
     <script>
         $(function() {
             $(document).pjax('[data-pjax] a, a[data-pjax]', '#pjax-container', {
                 timeout: 3000,
+            });
+
+            NProgress.configure({ showSpinner: false });
+
+            $(document).on('pjax:send', function() {
+                NProgress.start();
+            });
+
+            $(document).on('pjax:complete', function() {
+                NProgress.done();
             });
         });
     </script>
